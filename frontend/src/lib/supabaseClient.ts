2f12880ab3551136aea5deaf00e5116d8f02fcd4
VitaliyKalinichenko <veremij@ukr.net>
2025-08-02 23:17:58 +0300

    Update supabaseClient.ts

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ciupzfwuzkhzpzetrehl.supabase.co';  // <-- ВСТАВ сюди Project URL
-const supabaseAnonKey = 'sb_publishable_pNlhw3LimZ0UJSNXExTa7w_YYHPNC0H';                // <-- ВСТАВ сюди Publishable Key
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;                // <-- ВСТАВ сюди Publishable Key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
